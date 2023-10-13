da5ac28f09d258617bd8f91fd624b7f895764252
Kiranchandu Saragadam <110031953+kiranchandusaragadam@users.noreply.github.com>
2023-10-13 23:41:14 +0530

    method overriding done success

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,59 +1,30 @@
 package com.driver;
 
 public class Main {
-<<<<<<< HEAD
-    static class A{
-        public A(){
-
-        }
-        public String meth(){
-=======
-    public Main() {
-
-    }
-
-    public static class A {
-        public A() {
-
-        }
-
-        public String meth() {
->>>>>>> b3d1156b9f7cc31ff5ec3c645dfdb16f9c66923f
-            return "Invoking method from class A";
-        }
-    }
-
-<<<<<<< HEAD
-    static class B extends A{
-        public B(){
-
-        }
-        public String meth(){
-            return "Method is overridden in Extended class B";
-        }
-    }
     public Main(){
 
     }
-    public static void main(String[] args){
-        B b1 = new B();
-        System.out.println(b1.meth());
-=======
-    public static class B extends A {
-        public B() {
-
+    public static class A {
+        public A() {
         }
 
+        public String meth(){
+            return "Invoking method from class A";
+        }
+    }
+    public static class B extends A {
         @Override
         public String meth() {
-            return "Method is overridden in Extendend class B";
+            return "Method is overridden in Extended class B";
         }
     }
 
     public static void main(String[] args) {
-        B obj = new B();
-        String s = obj.meth();
+
+        B b = new B();
+        String s = b.meth();
         System.out.println(s);
->>>>>>> b3d1156b9f7cc31ff5ec3c645dfdb16f9c66923f
     }
 }
+
+
